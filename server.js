61357325db5f3c545b82f2d9154993440fae2f92
Asim-Sidd07 <mohammadasim.siddiqui@quadbtech.com>
2025-11-28 11:32:13 +0530

    9th'

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/api/users", userRoutes);
 
 const MONGO_URI = process.env.MONGO_URI;
